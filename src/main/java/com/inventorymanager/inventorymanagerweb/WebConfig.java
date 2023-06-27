77971d460ee23e78fd444e989462c3fa017b8597
Jonah Popp <jonah.popp@gmail.com>
2023-06-27 15:26:06 -0500

    testing all allowed origins

--- src/main/java/com/inventorymanager/inventorymanagerweb/WebConfig.java
+++ src/main/java/com/inventorymanager/inventorymanagerweb/WebConfig.java
@@ -16,7 +16,7 @@ public class WebConfig implements WebMvcConfigurer {
 @Override
 public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-            .allowedOrigins("http://localhost:3000", "https://gentle-spire-03025-64f5727df745.herokuapp.com")
+            .allowedOrigins("*")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
             .allowedHeaders("*")
             .exposedHeaders("Access-Control-Allow-Origin")
