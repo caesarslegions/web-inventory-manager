e510edf59c365cd513b16c5528211789f650f12c
Jonah Popp <jonah.popp@gmail.com>
2023-07-05 14:36:00 -0500

    Revert "deleted WebConfig.java"
    This reverts commit ff4e4a249d19dea398544524b41203844754e6f9.

---
+++ src/main/java/com/inventorymanager/inventorymanagerweb/WebConfig.java
@@ -0,0 +1,42 @@
+package com.inventorymanager.inventorymanagerweb;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+import java.io.IOException;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+@Override
+public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+        .allowedOrigins("https://gentle-spire-03025-64f5727df745.herokuapp.com") // change to this if you want to allow all origins, or ensure the correct origin is set
+        .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
+        .allowedHeaders("*")
+        .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
+        .allowCredentials(true);
+}
+
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+            .addResourceLocations("classpath:/static/")
+            .resourceChain(true)
+            .addResolver(new PathResourceResolver() {
+                @Override
+                protected Resource getResource(String resourcePath,
+                                               Resource location) throws IOException {
+                    Resource requestedResource = location.createRelative(resourcePath);
+                    return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
+                            : new ClassPathResource("/static/index.html");
+                }
+            });
+    }
+}
